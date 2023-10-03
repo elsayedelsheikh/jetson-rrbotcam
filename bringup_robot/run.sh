013530da4017dc70e969b7a1b543fdefd81535f0
Sayed <mec.sayed97@gmail.com>
2023-10-03 19:04:04 +0300

    Fixed an issue with building the container

--- bringup_robot/run.sh
+++ bringup_robot/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it --rm --network host --name rrbotcam_bringup --device /dev/ttyUSB0 elsayedelsheikh/jetson-rrbotcam-bringup-robot ros2 launch rrbot_cam rrbot.launch.py
+docker run -it --rm --network host --name rrbotcam_bringup --device /dev/ttyUSB0 elsayedelsheikh/jetson-rrbotcam-bringup-robot:latest ros2 launch rrbot_cam rrbot.launch.py
