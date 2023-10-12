e2f1adeabb159097ab7a910c6f0b9c7cbfe0a887
elsayedelsheikh <mec.sayed97@gmail.com>
2023-10-12 17:27:06 +0300

    Add PlatformController

--- bringup_robot/src/rrbot_cam_platform_controller/src/controller_main.cpp
+++ bringup_robot/src/rrbot_cam_platform_controller/src/controller_main.cpp
@@ -1,9 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
-#include "rrbot_cam_object_follower/ObjectFollower.hpp"
+#include "rrbot_cam_platform_controller/PlatfromController.hpp"
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<rrbot_cam_object_follower::ObjectFollower>();
+  auto node = std::make_shared<rrbot_cam_platform_controller::PlatfromController>();
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
