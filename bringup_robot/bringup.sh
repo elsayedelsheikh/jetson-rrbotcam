62cccc12a440f87bb3abe64bceac7d6f38aeb4cd
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2023-10-21 11:25:10 +0300

    ...

--- bringup_robot/bringup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -itd --rm --network host --name rrbotcam_bringup_robot --device /dev/ttyUSB0 elsayedelsheikh/jetson-rrbotcam-bringup-robot:latest ros2 launch rrbot_cam rrbot.launch.py
